cb4839aee2c53e85f9078de86e152a450b4056e2
ULLONGMAX <75794335+ULLONGMAX@users.noreply.github.com>
2024-07-03 16:07:29 -0400

    Create CFtemplate.cpp

---
+++ CFtemplate.cpp
@@ -0,0 +1,63 @@
+/*
+EL BICHO⠀⠀⠀⠀⣴⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⠂⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣿⣿⣿⣿⣿⣦⠀
+⠀⠀⠀⠀⠀⠀⣴⣿⢿⣷⠒⠲⣾⣾⣿⣿
+⠀⠀⠀⠀⣴⣿⠟⠁⠀⢿⣿⠁⣿⣿⣿⠻⣿⣄⠀⠀⠀⠀
+⠀⠀⣠⡾⠟⠁⠀⠀⠀⢸⣿⣸⣿⣿⣿⣆⠙⢿⣷⡀⠀⠀
+⣰⡿⠋⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⠀⠀⠉⠻⣿⡀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣆⠂⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⡿⣿⣿⣿⣿⡄⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⠿⠟⠀⠀⠻⣿⣿⡇⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⣾⡿⠃⠀⠀⠀⠀⠀⠘⢿⣿⡀⠀⠀⠀
+⠀⠀⠀⠀⠀⣰⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣷⡀⠀⠀
+⠀⠀⠀⠀⢠⣿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣧⠀⠀
+⠀⠀⠀⢀⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣆⠀
+⠀⠀⠠⢾⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣷⡤⠄
+¡SIIUUUUUUUUUUUUU!
+*/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+//typedef pair<int,int> ii;
+typedef double db;
+#define Bolivia_is_SIIUUUUUUUUUUUUU ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL)
+
+// bits:
+#define LSB(x) __builtin_ctz(x)
+#define LSBLL(x) __builtin_ctzll(x)
+#define MSB(x) 31-__builtin_clz(x)
+#define MSBLL(x) 63-__builtin_clzll(x)
+
+// printing & reading:
+#define NL cout<<'\n'
+template <typename T> void IMP(T x) {cout<<x;}
+template <typename T> void IMPL(T x) {IMP(x); NL;}
+template <typename T> void LEE(T &x) {cin>>x;}
+
+// vectors & tuples:
+#define pb push_back
+#define pob pop_back
+#define psf push_front
+#define pof pop_front
+#define V vector
+#define AR array
+
+// misc:
+template <typename T> void ASMIN(T &yo, T el) {yo=min(yo,el);}
+template <typename T> void ASMAX(T &yo, T el) {yo=max(yo,el);}
+
+void solve(){
+	
+}
+
+int main(){
+	Bolivia_is_SIIUUUUUUUUUUUUU;
+	
+	int t=1; cin>>t;
+	while(t--) solve();
+ 
+	return 0;
+}
+// Just a (very) hard conversation
